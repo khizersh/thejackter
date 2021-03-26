2fff63326bdae6c6c1ab0a963f4fa764ce171be1
Shahzaib <mohdshahzaib24@gmail.com>
2021-03-27 02:16:26 +0500

    added slider

--- src/Views/Home/index.jsx
+++ src/Views/Home/index.jsx
@@ -1,11 +1,25 @@
 import "./style.css";
-
+import SliderComponent from "../../Components/Slider";
+const slides = [
+    {
+        title: 'First item', description: 'Lorem ipsum', button: "Read More",
+        image: "https://i.imgur.com/ZXBtVw7.jpg",
+    },
+    {
+        title: 'Second item', description: 'Lorem ipsum', button: "Read More",
+        image: "https://i.imgur.com/ZXBtVw7.jpg",
+    },
+    {
+        title: 'Third item', description: 'Lorem ipsum', button: "Read More",
+        image: "https://i.imgur.com/ZXBtVw7.jpg",
+    }
+];
 const Home = () => {
-  return (
-    <div className="mt-5">
-      Home
-    </div>
-  );
+    return (
+        <div className="mt-4">
+            <SliderComponent slides={slides} />
+        </div>
+    );
 };
 
 export default Home;
