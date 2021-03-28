be38e288626e9902cc3f109416bb25d5cc60d822
MuhammdSheraz <mshazy999@gmail.com>
2021-03-28 11:38:37 +0500

    fix slider alignment

--- src/Components/Slider/index.jsx
+++ src/Components/Slider/index.jsx
@@ -1,25 +1,28 @@
 import Slider from "react-animated-slider";
-import 'react-animated-slider/build/horizontal.css';
+import "react-animated-slider/build/horizontal.css";
 
 import "./style.css";
 
 const SliderComponent = (props) => {
   return (
-    <Slider
-      autoplay={3000}
-    >
-      {props.slides && props.slides.map((item, index) => <div
-        key={index}
-        className="slider-content"
-        style={{ background: `url('${item.image}') no-repeat center center` }}
-      >
-        <div className="inner">
-          <h1>{item.title}</h1>
-          <p>{item.description}</p>
-          <button className="slider-button">{item.button}</button>
-        </div>
-      </div>)}
-    </Slider>
+      <Slider autoplay={3000}>
+        {props.slides &&
+          props.slides.map((item, index) => (
+            <div
+              key={index}
+              className="slider-content"
+              style={{
+                background: `url('${item.image}') no-repeat center center`,
+              }}
+            >
+              <div className="inner">
+                <h1>{item.title}</h1>
+                <p>{item.description}</p>
+                <button className="slider-button">{item.button}</button>
+              </div>
+            </div>
+          ))}
+      </Slider>
   );
 };
 
